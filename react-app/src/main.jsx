ef6fbda737ba80851851807648de86a7656a80ea
Siarhei <111513035+SiarheiBalabanovich@users.noreply.github.com>
2024-10-25 20:55:21 +0200

    Update main.jsx

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
 const reactRoot = createRoot(rootElement);
-createRoot.render(
+
+reactRoot.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
