0938251f0d3beec99fc3f19625317a006963af7b
Siarhei <s.balabanowicz@gmail.com>
2024-10-25 12:03:40 +0200

    Initial commit

---
+++ react-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Video from './Video/Video';
+import { VIDEOS } from './videos';
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='video-container'>
+      {/* <Video title="Video 2" channelName="Balabanovich JS" img={reactLogo} /> */}
+      {VIDEOS.map((video) => (
+          <Video
+          key={video.id}
+          title={video.title} 
+          channelName={video.channelName} 
+          img={video.img} 
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ react-app/src/videos.js
@@ -0,0 +1,52 @@
+import reactLogo from './assets/react.svg'
+
+export const VIDEOS = [
+    {
+        id: 1,
+        title: 'Video 1',
+        channelName: 'Balabanovich JS',
+        img: reactLogo,
+    },
+    {
+        id: 2,
+        title: 'Video 2',
+        channelName: 'Balabanovich JS',
+        img: reactLogo,
+    },
+    {
+        id: 3,
+        title: 'Video 3',
+        channelName: 'Balabanovich JS',
+        img: reactLogo,
+    },
+    {
+        id: 4,
+        title: 'Video 4',
+        channelName: 'Balabanovich JS',
+        img: reactLogo,
+    },
+    {
+        id: 5,
+        title: 'Video 5',
+        channelName: 'Balabanovich JS',
+        img: reactLogo,
+    },
+    {
+        id: 6,
+        title: 'Video 6',
+        channelName: 'Balabanovich JS',
+        img: reactLogo,
+    },
+    {
+        id: 7,
+        title: 'Video 7',
+        channelName: 'Balabanovich JS',
+        img: reactLogo,
+    },
+    {
+        id: 8,
+        title: 'Video 8',
+        channelName: 'Balabanovich JS',
+        img: reactLogo,
+    },
+];
